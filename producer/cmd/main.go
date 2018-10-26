6a51c2d19b95347adf04b051f21c9adc1e50e574
chrispaynes <cdpaynes@gmail.com>
2018-10-25 23:52:59 -0600

    adds twitter api config

--- producer/cmd/main.go
+++ producer/cmd/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"github.com/kelseyhightower/envconfig"
+	"log"
+)
+
+type Config struct {
+	ConsumerKey    string `split_words:"true"`
+	ConsumerSecret string `split_words:"true"`
+	AccessToken    string `split_words:"true"`
+	AccessSecret   string `split_words:"true"`
+}
 
 func main() {
-	fmt.Println("Test")
+	var c Config
+
+	err := envconfig.Process("TWITTER", &c)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
