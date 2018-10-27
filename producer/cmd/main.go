0fcae9e9728c50d45f1e54189fdd34871167b32c
chrispaynes <cdpaynes@gmail.com>
2018-10-27 11:04:47 -0600

    connects tweet producer to cassandra cluster

--- producer/cmd/main.go
+++ producer/cmd/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"github.com/gocql/gocql"
 	"github.com/kelseyhightower/envconfig"
-	"log"
+	log "github.com/sirupsen/logrus"
 )
 
 type Config struct {
@@ -13,11 +14,30 @@ type Config struct {
 }
 
 func main() {
+	log.SetFormatter(&log.JSONFormatter{})
+
 	var c Config
 
 	err := envconfig.Process("TWITTER", &c)
 
 	if err != nil {
 		log.Fatal(err.Error())
+	}
+
+	// connects to Cassandra using docker container name as hostname
+	cluster := gocql.NewCluster("cassandra")
+	cluster.Keyspace = "tweeter"
+	cluster.Consistency = gocql.Quorum
+	session, err := cluster.CreateSession()
+
+	if err != nil {
+		log.Fatalf("failed to initialize Cassandra cluster connection: %s", err.Error())
+	}
+
+	defer session.Close()
+
+	if err := session.Query(`insert into tweet(id, created_at, text) values (?, ?, ?)`,
+		gocql.TimeUUID(), "Wed Aug 27 13:08:45 +0000 2008", "Hello World").Exec(); err != nil {
+		log.Fatalf("failed to insert tweet into database: %s", err.Error())
 	}
 }
